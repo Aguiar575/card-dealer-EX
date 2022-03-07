1903bb90a8082651ffbe69da3d2e075d72119944
Aguiar575 <aguiarus99arthurus@gmail.com>
2022-03-06 23:11:32 -0300

    core methods added to project;

---
+++ lib/cards.ex
@@ -0,0 +1,47 @@
+defmodule Cards do
+  @moduledoc """
+   
+  """
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "four", "five"]
+    suits = ["spades", "clubs", "Hearts", "Diamonds"]
+
+    cards =
+      for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+      end
+
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File not exists!"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
+end
